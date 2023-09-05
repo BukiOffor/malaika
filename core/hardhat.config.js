c9fd27168794ac1e302c62714b8b341fcfc57005
Ebuka <ebuka2264@yahoo.com>
2023-09-05 15:34:57 +0100

    malaika:runs200

--- core/hardhat.config.js
+++ core/hardhat.config.js
@@ -34,6 +34,15 @@ module.exports = {
                     },
                 },
             },
+            {
+                version: "0.8.12",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            }
         ],
     },
     defaultNetwork: "hardhat",
